8bec13f3d54d03c8e2870848308045f56c8dce6e
damcio <d.p.koziol@gmail.com>
2023-04-17 23:44:43 +0200

    Event listeners added.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const daysView = document.getElementById("days");
+const hoursView = document.getElementById("hours");
+const minutesView = document.getElementById("minutes");
+const secondsView = document.getElementById("seconds");
+
 const newYears = "1 Jan 2024";
 
 function countdown() {
@@ -10,6 +15,11 @@ function countdown() {
   const hours = Math.floor((totalSeconds / 3600) % 24);
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
+
+  daysView.textContent = days;
+  hoursView.textContent = hours;
+  minutesView.textContent = minutes;
+  secondsView.textContent = seconds;
 }
 
 countdown();
